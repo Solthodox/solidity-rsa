5e0a46701234b2f708ed4d88593844f491aa4793
mrWojackETH <xabierblockchain@gmail.com>
2023-04-01 00:16:14 +0200

    first

---
+++ src/RSA.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: UNLICENSED
+/// @author : mrWojackETH
+pragma solidity ^0.8.19;
+import "forge-std/console.sol";
+
+contract RSA {
+    /// @notice generate public and private key
+    // Choose {p,q} prime numbers
+    // Choose {e} so that is also prime and is betweeen 0 and n=p*q
+    // The function generates {d} with the parameters
+    function keyGen(
+        uint256 p,
+        uint256 q,
+        uint256 e
+    ) external pure returns (bytes memory prKey, bytes memory pubKey) {
+        require(_isPrime(p), "p!=prime");
+        require(_isPrime(q), "q!=prime");
+        require(_isPrime(e), "e!=prime");
+        require(p >= 1 && q >= 1, "param<1");
+        // calculate phi(p,q) with Euler Totient function
+        uint256 phi = _phi(p, q);
+        uint256 n = p * q;
+        require((e > 1 && e < phi) && phi % n != 0, "e>1||e<phi||n mult e");
+        uint256 d = _getD(phi, e);
+        // d is the inverse of e
+        require((d * e) % phi == 1, "incorrect d");
+        //encode keys into a bytes to make it easier to manage(it can be decoded at any time)
+        prKey = abi.encode(d, n); // private key contains {d}(secret) and {n}(exposed in public key)
+        pubKey = abi.encode(e, n); // public key contains {e,n} and both are exposed
+    }
+
+    /// @notice Euler Totient function
+    /// it calculates the amount of numbers that are smaller than {n = p* q} that have no common factor
+    function _phi(uint256 p, uint256 q) private pure returns (uint256) {
+        /// can be easily calculated if {p} and {q} are given
+        return (p - 1) * (q - 1);
+    }
+
+    /// @notice compute the inverse to meet requirement : d * e % phi == 1, using Euler Totient algorithm
+    function _getD(uint256 c0, uint256 c1) private pure returns (uint256 d) {
+        int256[] memory c = new int256[](c1);
+        int256[] memory b = new int256[](c1);
+        bool found = false;
+        c[0] = int256(c0);
+        c[1] = int256(c1);
+        b[0] = 0;
+        b[1] = 1;
+        int256 t;
+        uint256 i = 1;
+        while (!found) {
+            c[i + 1] = c[i - 1] % c[i];
+            t = c[i - 1] / c[i];
+            b[i + 1] = b[i - 1] - (t * b[i]);
+            if (c[i + 1] == 1) {
+                found = true;
+                break;
+            }
+            i++;
+        }
+        d = b[i + 1] >= 0 ? uint256(b[i + 1]) : uint256(b[i + 1] + int256(c1));
+    }
+
+    function sign(
+        uint256 m,
+        bytes memory prKey
+    ) external pure returns (bytes memory mProof) {
+        (uint256 d, uint256 n) = abi.decode(prKey, (uint256, uint256));
+        uint256 rawMProof = _binExp(m, d, n);
+        mProof = abi.encode(rawMProof);
+    }
+
+    /*  function sign(
+        bytes memory c,
+        bytes memory prKey
+    ) external pure returns (bytes memory cProof) {
+        uint256 rawC = abi.decode(c, (uint));
+        (uint256 d, uint256 n) = abi.decode(prKey, (uint256, uint256));
+        uint256 rawCProof = _binExp(m, d, n);
+        cProof = abi.encode(rawCProof);
+    } */
+
+    function verify(
+        bytes memory proof,
+        bytes memory pubKey
+    ) external pure returns (uint256 m) {
+        (uint256 e, uint256 n) = abi.decode(pubKey, (uint256, uint256));
+        m = _binExp(abi.decode(proof, (uint256)), e, n);
+    }
+
+    /// @notice encrypt message with receiver's public key
+    /// @notice use binary exponentiation to handle huge numbers
+    function encrypt(
+        uint256 m,
+        bytes memory pubKey
+    ) external pure returns (bytes memory c) {
+        // decode public key to get {e,n}
+        (uint256 e, uint256 n) = abi.decode(pubKey, (uint256, uint256));
+        // cepherText = message ** e % n
+        uint256 rawC = _binExp(m, e, n);
+        // encode cepherText
+        c = abi.encode(rawC);
+    }
+
+    /// @notice computate x**pow % mod usind binary exponentiation
+    function _binExp(
+        uint256 _x,
+        uint256 _pow,
+        uint256 _mod
+    ) private pure returns (uint256) {
+        uint256 res = 1;
+        _x = _x % _mod;
+        if (_x == 0) return 0;
+
+        while (_pow > 0) {
+            if (_pow % 2 == 1) res = (res * _x) % _mod;
+            _pow = _pow >> 1;
+            _x = (_x * _x) % _mod;
+        }
+        return res;
+    }
+
+    /// @notice decrypt message with own private key
+    function decrypt(
+        bytes memory c,
+        bytes memory prKey
+    ) external pure returns (uint256 m) {
+        (uint256 d, uint256 n) = abi.decode(prKey, (uint256, uint256));
+        uint256 rawC = abi.decode(c, (uint256));
+        m = _binExp(rawC, d, n);
+    }
+
+    /// @notice square root function
+    function _sqrt(uint256 _y) private pure returns (uint z) {
+        if (_y > 3) {
+            z = _y;
+            uint x = _y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (_y / x + x) / 2;
+            }
+        } else if (_y != 0) {
+            z = 1;
+        }
+    }
+
+    /// @notice check if a number is prime
+    function _isPrime(uint256 _n) private pure returns (bool) {
+        if (_n <= 1) return false;
+        for (uint256 i = 2; i < _sqrt(_n) + 1; i++) {
+            if (_n % i == 0) return false;
+        }
+        return true;
+    }
+}
---
+++ test/RSA.t.sol
@@ -0,0 +1,92 @@
+pragma solidity ^0.8.19;
+import "forge-std/Test.sol";
+import "../src/RSA.sol";
+
+contract RSATest is Test {
+    address public constant user1 = address(0x1);
+    address public constant user2 = address(0x2);
+    RSA public rsa;
+
+    function setUp() public {
+        rsa = new RSA();
+    }
+
+    // we preselect {p,q,e} because its computationally expensive to generate big random prime numbers that meet the requirements
+    function testKeyGen()
+        public
+        returns (bytes memory prKey, bytes memory pubKey)
+    {
+        // generate private and public key
+        (prKey, pubKey) = rsa.keyGen(571, 619, 211);
+        //decode them
+        (uint256 d, uint256 n1) = abi.decode(prKey, (uint256, uint256));
+        (uint256 e, uint256 n2) = abi.decode(pubKey, (uint256, uint256));
+        // assert prKey(d,n) is (156931, 353449)
+        assertEq(d, 156931);
+        assertEq(n1, 353449);
+        // assert pubKey(e,n) is (211, 353449)
+        assertEq(e, 211);
+        assertEq(n2, 353449);
+        console.log("d :", d, " n : ", n1);
+        console.log("e :", e, " n : ", n2);
+    }
+
+    function testSendMessages(uint256 m) public {
+        //the message "m" will be a random uint so that {1 < m < 10000000}
+        vm.assume(m > 1 && m < 100000);
+        //user 1 generates keys
+        vm.startPrank(user1);
+        (bytes memory pr1, bytes memory pub1) = rsa.keyGen(571, 619, 211);
+        vm.stopPrank();
+        //user 2 gnerates keys
+        vm.startPrank(user2);
+        (bytes memory pr2, bytes memory pub2) = rsa.keyGen(2287, 2371, 2357);
+        vm.stopPrank();
+        //user1 encrypts message for user 2
+        vm.startPrank(user1);
+        bytes memory cepherText = rsa.encrypt(m, pub2); //uses user 2's public key
+        vm.stopPrank();
+        //decrypt is as user2 using own private key
+        vm.startPrank(user2);
+        uint256 m_ = rsa.decrypt(cepherText, pr2);
+        console.log("DECRYPTED : ", m_);
+        vm.stopPrank();
+        //make sure the decrypted message equals the original message "m"
+        assertEq(m_, m, "m!=m");
+        // if we decrypt it with any other private key we get a wrong message
+        uint256 wrongMessage = rsa.decrypt(cepherText, pr1);
+        console.log("wrong : ", wrongMessage);
+        assertFalse(wrongMessage == m);
+    }
+
+    // sign a message with a private key and verify that person signed the message with his public key
+    function testSignTransparentMesssage(uint256 m) public {
+        vm.assume(m > 1 && m < 1000000);
+        rsa.keyGen(43261, 43313, 43291);
+        (bytes memory prKey, bytes memory pubKey) = rsa.keyGen(571, 619, 211);
+        bytes memory proof = rsa.sign(m, prKey);
+        uint256 verified = rsa.verify(proof, pubKey);
+        assertEq(m, verified);
+    }
+
+    function _isPrime(uint256 n) private pure returns (bool) {
+        if (n <= 1) return false;
+        for (uint256 i = 2; i < _sqrt(n) + 1; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    function _sqrt(uint256 _y) private pure returns (uint z) {
+        if (_y > 3) {
+            z = _y;
+            uint x = _y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (_y / x + x) / 2;
+            }
+        } else if (_y != 0) {
+            z = 1;
+        }
+    }
+}
